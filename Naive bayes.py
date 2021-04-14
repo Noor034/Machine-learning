4c30f7f8ac79654c9a7d60e8846a4c13c879f57d
noor170204034 <42875025+noor170204034@users.noreply.github.com>
2021-04-14 16:29:11 +0600

    update

---
+++ Naive bayes.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 14 15:12:11 2021
+
+@author: User
+"""
+
+import numpy as np 
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+
+#importing data set
+
+dataset = pd.read_csv('Social_Network_ads.csv')
+
+X = dataset.iloc[:, [2,3]].values
+#X =X.reshape(-1,1)
+Y = dataset.iloc[:,4].values
+#Y = Y.reshape (-1,1)
+
+from sklearn.model_selection import train_test_split
+
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=.25 ,random_state = 0)
+
+
+from sklearn.preprocessing import StandardScaler
+
+sc_X = StandardScaler()
+
+X = sc_X.fit_transform(X)
+
+
+#naive classifier
+
+from sklearn.naive_bayes import GaussianNB
+
+classifier = GaussianNB()
+classifier.fit(X_train,Y_train)
+
+
+Y_pred = classifier.predict(X_test)
+
+
+from sklearn.metrics import confusion_matrix
+
+cm = confusion_matrix(Y_test, Y_pred)
+
+
+from matplotlib.colors import ListedColormap
+X_set, Y_set = X_train, Y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Naive (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show() 
+
---
+++ Random forest classifier.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 14 16:22:55 2021
+
+@author: User
+"""
+
